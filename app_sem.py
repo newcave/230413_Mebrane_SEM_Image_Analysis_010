a48d4a1fcf42d9db45bba4aff3e933e431dfdf86
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2023-04-13 16:54:27 +0900

    Update app_sem.py

--- app_sem.py
+++ app_sem.py
@@ -1,1 +1,66 @@
+import streamlit as st
+from PIL import Image, ImageOps
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    st.title("Membrane Pore Reduction Analysis")
+
+    st.sidebar.header("Input Images")
+    img1 = st.sidebar.file_uploader("Upload Image 1:", type=["png", "jpg", "jpeg"])
+    img2 = st.sidebar.file_uploader("Upload Image 2:", type=["png", "jpg", "jpeg"])
+    img3 = st.sidebar.file_uploader("Upload Image 3:", type=["png", "jpg", "jpeg"])
+    img4 = st.sidebar.file_uploader("Upload Image 4:", type=["png", "jpg", "jpeg"])
+    img5 = st.sidebar.file_uploader("Upload Image 5:", type=["png", "jpg", "jpeg"])
+
+    st.sidebar.header("Threshold")
+    threshold = st.sidebar.slider("Select a threshold value:", min_value=0, max_value=255, value=50, step=1)
+
+    if img1 and img2 and img3 and img4 and img5:
+        img1 = Image.open(img1)
+        img2 = Image.open(img2)
+        img3 = Image.open(img3)
+        img4 = Image.open(img4)
+        img5 = Image.open(img5)
+        
+        process_images(img1, img2, img3, img4, img5, threshold)
+
+def process_images(img1, img2, img3, img4, img5, threshold):
+    # Resize all images to match the size of img1
+    width, height = img1.size
+    img2 = img2.resize((width, height))
+    img3 = img3.resize((width, height))
+    img4 = img4.resize((width, height))
+    img5 = img5.resize((width, height))
+
+    # Print the threshold value
+    st.write(f"THRESHOLD OF PORE REPRESENTATION: {threshold}")
+
+    # Calculate the number of black pixels in each image based on the threshold
+    # ...
+
+    # Calculate the black pixel ratio for each image
+    # ...
+
+    # Compute the differences in black pixel ratios between the original and processed images
+    # ...
+
+    # Call the plot_images() function to visualize the images and their pore ratios
+    plot_images(img_list, black_ratios, black_ratio_diffs, threshold)
+
+def plot_images(img_list, black_ratios, black_ratio_diffs, threshold):
+    # Plot the images in a row
+    # ...
+
+    # Draw bar charts to show the pore ratio differences for each image
+    # ...
+
+    # Plot line graphs to display the change in pore reduction for each image
+    # ...
+
+    # Show the plots
+    plt.show()
+
+if __name__ == "__main__":
+    main()
 
